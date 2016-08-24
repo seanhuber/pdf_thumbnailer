b8e76b54a039fa7e9db3c50122b242c97e32ce51
Sean Huber <seanhuber@seanhuber.com>
2016-08-24 15:45:32 -0500

    convert pdf pages to png images using pdftoppm

--- lib/pdf_thumbnailer/pdf_thumbs.rb
+++ lib/pdf_thumbnailer/pdf_thumbs.rb
@@ -1,9 +1,39 @@
 class PdfThumbs
   def self.configure **opts
-    [:pdf_dir, :img_dir].each do |required_opt|
+    [:pdf_dir, :img_dir, :thumb_sizes].each do |required_opt|
       raise ArgumentError, "Missing required config option: :#{required_opt}" unless opts[required_opt]
     end
     @@pdf_dir = File.join(opts[:pdf_dir], '') # adds trailing '/' if it doesn't already have one
     @@img_dir = File.join(opts[:img_dir], '') # adds trailing '/' if it doesn't already have one
+    @@thumb_sizes = opts[:thumb_sizes].is_a?(Fixnum) ? [opts[:thumb_sizes]] : opts[:thumb_sizes]
+  end
+
+  def self.thumbnail!
+    Dir.glob(File.join(@@pdf_dir, '**', '*.pdf')) do |pdf|
+      img_dir = clear_img_dir pdf
+
+      @@thumb_sizes.each do |thumb_size|
+        `pdftoppm -png -scale-to #{thumb_size} "#{pdf}" "#{img_dir}"`
+
+        Dir.glob(File.join(img_dir, '-*.png')) do |img_path|
+          page_num = File.basename(img_path, '.png').split('-')[-1].to_i
+          File.rename(img_path, File.join(img_dir, "#{thumb_size}_#{page_num}.png"))
+        end
+      end
+      yield(img_dir) if block_given?
+    end
+  end
+
+  private
+
+  def self.clear_img_dir pdf
+    relative_path = File.dirname pdf.gsub(@@pdf_dir, '')
+    relative_img_path = relative_path == '.' ? File.basename(pdf) : File.join(relative_path, File.basename(pdf))
+    img_dir = File.join(@@img_dir, relative_img_path, '')
+
+    FileUtils.rm_rf(img_dir) if File.directory?(img_dir)
+    FileUtils.mkdir_p img_dir
+
+    img_dir
   end
 end
--- spec/pdf_thumbs_spec.rb
+++ spec/pdf_thumbs_spec.rb
@@ -14,9 +14,44 @@ describe PdfThumbs do
 
     expect {
       PdfThumbs.configure(
+        pdf_dir: '/some/path/of/pdfs',
+        img_dir: '/some/path/of/pdfs'
+      )
+    }.to raise_error 'Missing required config option: :thumb_sizes'
+
+    expect {
+      PdfThumbs.configure(
         img_dir: '/some/path/of/images',
-        pdf_dir: '/some/path/of/pdfs'
+        pdf_dir: '/some/path/of/pdfs',
+        thumb_sizes: [1000, 500]
       )
     }.not_to raise_error
+  end
+
+  it 'can generate pdf page thumbnails' do
+    img_dir = File.expand_path('../thumbnails', __FILE__)
+    thumb_sizes = [1000, 500]
+    PdfThumbs.configure(
+      pdf_dir: File.expand_path('../pdfs', __FILE__),
+      img_dir: img_dir,
+      thumb_sizes: thumb_sizes
+    )
+    expected_dirs = [
+      File.join(File.expand_path('../thumbnails/First.pdf', __FILE__), ''),
+      File.join(File.expand_path('../thumbnails/Second.pdf', __FILE__), '')
+    ]
+    idx = 0
+    PdfThumbs.thumbnail! do |thumb_dir|
+      expect(thumb_dir).to eq(expected_dirs[idx])
+      expect(File).to be_directory(thumb_dir)
+      (1..3).each do |page_num| # each file has 3 pages
+        thumb_sizes.each do |thumb_size|
+          expect(File).to exist(File.join(thumb_dir, "#{thumb_size}_#{page_num}.png"))
+        end
+      end
+      idx += 1
+    end
+
+    FileUtils.rm_rf img_dir
   end
 end
